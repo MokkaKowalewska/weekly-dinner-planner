138f60abc2b143d2c69b6fda111610f20da861a3
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-24 00:17:39 +0200

    refactor: clean code, correct $content area

--- src/send-email.php
+++ src/send-email.php
@@ -1,22 +1,23 @@
 <?php
 
 
-ini_set("SMTP", "serwer1940016.home.pl ");
+ini_set("SMTP", "serwer1940016.home.pl");
 ini_set("sendmail_from", "hello@monikakowalewska.pl");
 ini_set("smtp_port", "465");
 
 
-if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
-    $name = $_POST["name"];
+if(isset($_POST["email"]) && isset($_POST["message"])) {
     $email = $_POST["email"];
     $message = nl2br($_POST["message"]);
     $to = $email;
     $from = "hello@monikakowalewska.pl";
-    $subject = "Weekly Dinner Planner - Your dinner plan for this week;
-    $content = "<h1>weekly dinner planner</h1>" . "<h2>Hi! Here is Your dinner plan for this week:</h2><br><br>" . $message . "<img src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" alt="food photo">" . "<br>Thanks for using Weekly Dinner Planner. Have a wonderful dinner! Hope to see You soon!";
-    $headers = "From: $from\n";
-    $headers .= "MIME-Version: 1.0\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+    $subject = "Weekly Dinner Planner - Your dinner plan for this week";
+    $content = "<h1>weekly dinner planner</h1><h2>Hi! Here is Your dinner plan for this week:</h2><br><br>" . $message;
+    $content .= "obrazek<br>Thanks for using Weekly Dinner Planner. Have a wonderful dinner! Hope to see You soon!";
+    $headers = "From: $from\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+
 
         if(mail($to, $subject, $content, $headers))  {
             echo "success";
