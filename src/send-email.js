bc46273756d8ef32a07886c94d36eba2af72108e
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-23 07:51:48 +0200

    feat: add variables, start onsubmit fn

---
+++ src/send-email.js
@@ -0,0 +1,10 @@
+const form = document.querySelector(".form");
+const email = document.querySelector(".form__email");
+const submitBtn = document.querySelector(".form__btn");
+
+
+form.addEventListener("submit", () => {
+
+
+},
+false);
