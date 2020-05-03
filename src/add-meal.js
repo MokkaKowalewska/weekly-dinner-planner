b87ff379ed7fdf7fc33ea45967897c885265eaeb
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-03 22:55:40 +0200

    refactor: rebuild CSS classes to box, btn and input

--- src/add-meal.js
+++ src/add-meal.js
@@ -15,14 +15,14 @@ function addMeal(text) {
   ul.insertAdjacentHTML(
     "beforeend",
     `
-  <li class="favMeals__li box" data-key="${meal.id}">
+  <li class="favMeals__li box animated fadeInDown" data-key="${meal.id}">
     <span class="li__span">${meal.text}</span>
     <svg class="favMeals__deleteBtn" aria-label="Delete button" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg>
   </li>
   `,
   );
 
-  const selects = document.querySelectorAll(".selectMeal");
+  const selects = document.querySelectorAll(".select");
   selects.forEach((select) => {
     select.insertAdjacentHTML(
       "beforeend",
